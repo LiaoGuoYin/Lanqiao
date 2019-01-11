a0ef78536c49482b3929cf98aa7eae40d774463c
LiaoGuoYin <liaoguoyin@live.com>
2019-01-12 00:29:38 +0800

    0入门训练

--- Lanqiao/入门训练/A4.java
+++ Lanqiao/入门训练/A4.java
@@ -1,24 +1,46 @@
 //Fobinacci数列
-
 import java.util.Scanner;
 
-
-public class Main{
-	public static void main(String[] args){
+public class A4 {
+	public static void main(String[] args) {
 		Scanner sc1 = new Scanner(System.in);
 		int n = sc1.nextInt();
 		sc1.close();
 
-		long[] F = new long[n+2];
-		F[1]=F[2]=1;
-		for(int i=3;i<=n;i++){
-			F[i] = (F[i-1]+F[i-2])%10007;
+		long[] F = new long[n + 2];
+		F[1] = F[2] = 1;
+		for (int i = 3; i <= n; i++) {
+			F[i] = (F[i - 1] + F[i - 2]) % 10007;
 		}
 
 		System.out.print(F[n]);
 	}
 }
 
-//错了好几次,全在数组越界上..
-//看了下lichuo,才发现,可以暴力先迭代出所有F[1000000]然后直接对F[n]操作
-//TODO 这里有点没懂对每一个F[i]先取余再赋值，隐约不是Fobinacci数列的感觉 但是这里已经没有足够大的储存F[1000000]的数据类型了 先写个坑..
+// 法二：
+public class A4_2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		scanner.close();
+		long[] window = new long[] { 1, 1, 0 };
+
+		// 处理前两个斐波那楔数
+		if (n == 1) {
+			System.out.println("1");
+		}
+		if (n == 2) {
+			System.out.println("1 1");
+		}
+
+		for (int i = 3; i <= n; i++) {
+			window[2] = window[0] + window[1];
+			System.out.print(" " + window[2]);
+
+			window[0] = window[1];
+			window[1] = window[2];
+		}
+	}
+}
+// 错了好几次,全在数组越界上，这个得注意..
+// 法二是真的牛皮，窗口滑动的思想，就只取局部的3个窗口，用偏移的方法节省了空间 666
