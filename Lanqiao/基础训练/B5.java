da1abfa24d3effdbb6e00dba8e12cbeef8988bf7
LiaoGuoYin <liaoguoyin@live.com>
2019-01-11 23:02:36 +0800

    查找整数

---
+++ Lanqiao/基础训练/B5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class B5{
+	public static void main(String[] args) {
+		Scanner scanner =new Scanner(System.in);
+		int n =scanner.nextInt();
+		int[] arr1 = new int[n];
+		
+		for(int i = 0;i<n;i++) {
+			arr1[i] = scanner.nextInt();
+		}
+		
+		scanner.nextLine();
+		int a = scanner.nextInt();
+		int location_a = -1;
+		
+		for(int i = 0;i<arr1.length;i++) {
+			if(arr1[i] == a) {
+				location_a = i+1;
+				break;
+			}
+		}
+		System.out.print(location_a);
+	}
+}
+
+//这里有个坑:输入第二行数据之后,要先nextLine()以换行符截断一下,不然最后输入的待查数a获取不到
