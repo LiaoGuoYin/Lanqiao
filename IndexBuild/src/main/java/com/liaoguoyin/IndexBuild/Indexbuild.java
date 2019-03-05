7f53836195be61c21b026c5bcb1e9b3eea475afd
LiaoGuoYin <liaoguoyin@live.com>
2019-03-05 23:37:07 +0800

    update README

--- IndexBuild/src/main/java/com/liaoguoyin/IndexBuild/Indexbuild.java
+++ IndexBuild/src/main/java/com/liaoguoyin/IndexBuild/Indexbuild.java
@@ -6,7 +6,10 @@ import java.util.Arrays;
 
 public class Indexbuild {
     public static void main(String[] args) throws IOException {
-        PrintWriter printWriter = new PrintWriter("./directoryIndex.txt", "UTF-8");
+        File file0 = new File("README.md");
+        FileWriter fileWriter = new FileWriter(file0, true);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+
         File file = new File("PAT/src/main/kotlin/com/hiczp/pat/gplt");
         File[] files = file.listFiles();
         Arrays.sort(files);
@@ -15,12 +18,14 @@ public class Indexbuild {
         String[] title = new String[files.length];
         String[] javaPath = new String[files.length];
         for (int i = 0; i < files.length; i++) {
-            id[i] = files[i].getName().split(" ", 0)[0];
-            title[i] = files[i].getName().split(" ", 0)[1].split("\\.kt")[0];// 匹配不完整，正则??TOdo
-            javaPath[i] = String.format("PAT/src/main/java/com/liaoguoyin/pat/团体程序设计天梯赛/%s%s", id[i].split("-")[0], id[i].split("-")[1]);
+            String tmp = files[i].getName();
+            id[i] = tmp.substring(0, 6);
+            title[i] = tmp.substring(7, tmp.length() - 3);
+            javaPath[i] = String.format("PAT/src/main/java/com/liaoguoyin/pat/团体程序设计天梯赛/%s%s/Main.java", id[i].split("-")[0], id[i].split("-")[1]);
             String result = String.format("| %s | %s | [Java](%s) | [Kotlin](%s) |%n", id[i], title[i], urlencode(javaPath[i]), urlencode(files[i].getPath()));
 
-            // 输出到文件
+            // 追加输出到READ.ME
+            System.out.println(result);
             printWriter.print(result);
         }
         printWriter.close();
