c512af25e36fe9ad0c6ae1e48fd44068c0de0a58
LiaoGuoYin <liaoguoyin@live.com>
2019-03-06 15:13:06 +0800

    数据结构 / java

---
+++ dataStructure/src/main/java/com/liaoguoyin/dataStructure/LinkedList.java
@@ -0,0 +1,122 @@
+package com.liaoguoyin.dataStructure;
+
+
+/**
+ * 链表LinkedList
+ * 实现了：增加节点、删除节点、输出所有元素（同时删除）、test
+ * 注意体会：链表的head和tail都是实实在在的节点（似乎是废话..）
+ */
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    // test
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.addHead("111");
+        linkedList.addHead("000");
+        linkedList.display();
+        linkedList.addTail("222");
+        linkedList.addTail("333");
+        linkedList.addTail("444");
+        linkedList.addTail("555");
+        linkedList.deleteHead();
+        linkedList.deleteTail();
+        linkedList.display();
+    }
+
+    /**
+     * 增加头节点
+     *
+     * @param object
+     */
+    public void addHead(Object object) {
+        Node node = new Node(object);
+        if (size == 0) {
+            head = node;
+            tail = node;
+            size++;
+        } else {
+            head.prev = node;
+            node.next = head;
+            head = node;
+            size++;
+        }
+    }
+
+    /**
+     * 删除头节点
+     */
+    public void deleteHead() {
+        if (size != 0) {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    /**
+     * 在尾节点添加元素
+     *
+     * @param object
+     */
+    public void addTail(Object object) {
+        Node node = new Node(object);
+        if (size == 0) {
+            tail = node;
+            head = node;
+            size++;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+            size++;
+        }
+    }
+
+    /**
+     * 删除尾节点
+     */
+    public void deleteTail() {
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+    }
+
+    /**
+     * 输出所有元素（同时删除）
+     */
+    public void display() {
+        Node node = head;
+        while (size > 0) {
+            if (size == 1) {
+                System.out.println(node.data);
+            } else {
+                System.out.print(node.data + " -> ");
+            }
+            node = node.next;
+            size--;
+        }
+    }
+
+
+    /**
+     * 双端链表
+     */
+    public class Node {
+        private Node prev;
+        private Object data;
+        private Node next;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+    }
+}
---
+++ dataStructure/src/main/java/com/liaoguoyin/dataStructure/Queue.java
@@ -0,0 +1,104 @@
+package com.liaoguoyin.dataStructure;
+
+/**
+ * 单向队列Queue
+ * 实现了：构造方法、入队、出队、顺序存储结构实现Queue、链式存储结构实现Queue
+ * （队头队尾都是用一个变量来指向，队列元素本身的地址不发生改变）
+ */
+public class Queue {
+    private Object[] objectQueue;
+    private int size;
+    private int top;
+    private int bottom;
+    private int item;
+    private NodeQueue queueTop;
+    private NodeQueue queueBottom;
+    private int queueSize;
+
+
+    // 顺序存储结构实现Queue
+    public Queue(int size) {
+        this.size = size;
+        top = 0;
+        bottom = -1;
+        objectQueue = new Object[size];
+    }
+
+    public Queue() {
+        queueBottom = null;
+        queueTop = null;
+        size = 0;
+    }
+
+    /**
+     * 入队
+     *
+     * @param object
+     */
+    public void enQueue(Object object) {
+        if (item == size) {
+            throw new RuntimeException("Queue is full!!");
+        }
+        // 循环队列，首尾组合
+        if (bottom == size - 1) {
+            bottom = -1;
+        }
+        objectQueue[++item] = object;
+        top++;
+    }
+
+    /**
+     * 出队
+     *
+     * @return 返回下标为top的元素
+     */
+    public Object outQueue() {
+        if (item == 0) {
+            throw new RuntimeException("Queue is void");
+        }
+        Object obj = objectQueue[top];
+        top++;
+
+        if (top == size) {
+            top = 0;
+        }
+        item--;
+        return obj;
+    }
+
+    public void addNodeQueue(Object obj) {
+        if (size == 0) {
+            queueTop = new NodeQueue(obj, null);
+            queueBottom = queueTop;
+        } else {
+            NodeQueue<Object> nodeQueue = new NodeQueue(obj, null);
+            queueBottom.next = nodeQueue;
+            queueBottom = nodeQueue;
+        }
+        size++;
+    }
+
+    public Object removeQueue() {
+        if (size == 0) {
+            throw new RuntimeException("Queue is null");
+        }
+        NodeQueue nodeQueue = queueTop;
+        queueTop = queueTop.next;
+        nodeQueue.next = null;
+
+        size--;
+        return nodeQueue.data;
+    }
+
+    // 链式存储结构实现Queue
+    private class NodeQueue<Object> {
+        private Object data;
+        private NodeQueue next;
+
+        public NodeQueue(Object data, NodeQueue next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+}
